149b746371f87e0076c5aa02f311c4f32d461b2c
Vitaliia Ioffe <vitaioffe@yandex.ru>
2026-02-22 16:26:58 +0200

    Refactor loaders

--- src/hedging_pipeline/__init__.py
+++ src/hedging_pipeline/__init__.py
@@ -5,14 +5,25 @@ NASDAQ-100 rebalancing event pipeline: load, classify, enrich, summarize.
 from hedging_pipeline.classification import EventClassifier
 from hedging_pipeline.config import PipelineConfig
 from hedging_pipeline.enrichment import PriceEnricher
-from hedging_pipeline.loaders import DataQualityError, EventLoader
+from hedging_pipeline.loaders import (
+    BaseLoader,
+    DataQualityError,
+    DailyBarsLoader,
+    EventLoader,
+    EventsLoader,
+    PipelineLoader,
+)
 from hedging_pipeline.pipeline import Pipeline
 from hedging_pipeline.summary import SummaryStats
 
 __all__ = [
     "PipelineConfig",
+    "BaseLoader",
     "DataQualityError",
+    "DailyBarsLoader",
     "EventLoader",
+    "EventsLoader",
+    "PipelineLoader",
     "EventClassifier",
     "PriceEnricher",
     "SummaryStats",
