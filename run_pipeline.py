0fff6a1d59e7316de9a2b7c7330d17e85615b0e3
Vitaliia Ioffe <vitaioffe@yandex.ru>
2026-02-22 14:57:06 +0200

    Add main runner

---
+++ run_pipeline.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Entry point: run the event processing pipeline (uses package and logging.ini).
+"""
+from hedging_pipeline.cli import main
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
